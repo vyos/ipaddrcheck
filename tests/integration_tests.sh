2e3b76bff7717336af9614cce169e6cc0dcafea9
Daniil Baturin <daniil@baturin.org>
2013-05-31 07:35:30 -0700

    Remove fake failure from integration tests.

--- tests/integration_tests.sh
+++ tests/integration_tests.sh
@@ -30,7 +30,7 @@ assert_raises "$IPADDRCHECK --is-valid 192.0.2.1/24"   0
 assert_raises "$IPADDRCHECK --is-valid 192.0.2.0/26"   0
 assert_raises "$IPADDRCHECK --is-valid 2001:db8::1"    0
 assert_raises "$IPADDRCHECK --is-valid 2001:db8::/56"  0
-assert_raises "$IPADDRCHECK --is-valid 192.0.2.666"    0
+assert_raises "$IPADDRCHECK --is-valid 192.0.2.666"    1
 assert_raises "$IPADDRCHECK --is-valid garbage"        1
 
 assert_end ipaddrcheck_integration
